22c5cea239eeef68eb651e1de41c618b5b57f5f7
AwesomestCode <53128754+AwesomestCode@users.noreply.github.com>
2023-01-10 17:40:34 -0500

    alignment demo untested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlignmentDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AlignmentDemo.java
@@ -2,12 +2,21 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDrivetrainMixer;
 import org.firstinspires.ftc.teamcode.subsystems.TapePositionSensor;
 
 @TeleOp(group="Demo")
 public class AlignmentDemo extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        DcMotorEx frontLeft = (DcMotorEx) hardwareMap.dcMotor.get("frontLeft");
+        DcMotorEx frontRight = (DcMotorEx) hardwareMap.dcMotor.get("frontRight");
+        DcMotorEx rearLeft = (DcMotorEx) hardwareMap.dcMotor.get("rearLeft");
+        DcMotorEx rearRight = (DcMotorEx) hardwareMap.dcMotor.get("rearRight");
+
+        MecanumDrivetrainMixer mixer = new MecanumDrivetrainMixer(frontLeft, frontRight, rearLeft, rearRight);
+
         TapePositionSensor sensor = new TapePositionSensor(hardwareMap, TapePositionSensor.TapeColour.RED);
         waitForStart();
 
@@ -17,8 +26,12 @@ public class AlignmentDemo extends LinearOpMode {
 
             if (gamepad1.touchpad) {
                 telemetry.addData("Touchpad", "Pressed");
+                telemetry.addData("Suggested Power", sensor.getSuggestedPower());
+                mixer.setMovement(sensor.getSuggestedPower(), 0, 0);
+
             } else {
                 telemetry.addData("Touchpad", "Not Pressed");
+                mixer.setMovement(0, 0,0);
             }
 
             telemetry.update();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TapePositionSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TapePositionSensor.java
@@ -1,8 +1,17 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import java.util.Timer;
+import java.util.function.DoubleSupplier;
+
+@Config
 public class TapePositionSensor {
+    public static double kp = 0.5;
+    public static double ki = 0;
+    public static double kd = 0;
     public enum TapeColour {
         RED,
         BLUE
@@ -11,11 +20,13 @@ public class TapePositionSensor {
     private ColourSensor leftSensor;
     private ColourSensor rightSensor;
     private TapeColour colour;
+    private PIDController pidController;
 
     public TapePositionSensor(HardwareMap map, TapeColour colour) {
         leftSensor = new ColourSensor(map, "cs0");
         rightSensor = new ColourSensor(map, "cs1");
         this.colour = colour;
+        pidController = new PIDController(this::getPosition);
     }
 
     public float getPosition() {
@@ -24,5 +35,61 @@ public class TapePositionSensor {
         } else {
             throw new UnsupportedOperationException("Blue tape not supported yet");
         }
+    }
+
+    public double getSuggestedPower() {
+        return pidController.getOutput();
+    }
+
+    public static class PIDController {
+        //private double kp;
+        //private double ki;
+        //private double kd;
+        private double lastError;
+        private double integral;
+        private double derivative;
+        private static final double SETPOINT = 0;
+
+        DoubleSupplier input;
+
+        /*public PIDController(double kp, double ki, double kd) {
+            this.kp = kp;
+            this.ki = ki;
+            this.kd = kd;
+        }*/
+
+        private ElapsedTime timer = new ElapsedTime();
+
+        public PIDController(DoubleSupplier input) {
+            timer.reset();
+            lastError = SETPOINT - input.getAsDouble();
+            this.input = input;
+            Timer timer = new Timer();
+            timer.schedule(new java.util.TimerTask() {
+                @Override
+                public void run() {
+                    updateValues();
+                }
+            }, 0, 10);
+        }
+
+
+        public void updateValues() {
+            // Calculate error
+            double error = SETPOINT - input.getAsDouble();
+            // Calculate integral
+            integral = this.integral + timer.nanoseconds() * error;
+            // Calculate derivative
+            derivative = (error - lastError) / timer.nanoseconds();
+            // Set last error
+            lastError = error;
+            timer.reset();
+        }
+
+
+        public double getOutput() {
+            return kp * lastError + ki * integral + kd * derivative;
+        }
+
     }
 }
